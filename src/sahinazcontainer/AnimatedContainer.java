068ad4076de8375c5d09c44ffa9b28e4a4c2a6d1
Lakaemper <rlakaemper@gmail.com>
2017-11-10 19:40:54 +0100

    AnimatedContainer created

---
+++ src/sahinazcontainer/AnimatedContainer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sahinazcontainer;
+
+import java.awt.Point;
+/**
+ *
+ * @author rolflakaemper
+ */
+public class AnimatedContainer {
+    Point currentPosition;
+    Point targetPosition;
+    
+    public AnimatedContainer(){
+        
+    }
+    
+    private Point positionToGrid(Point p){
+        
+    }
+    
+    
+    
+}
